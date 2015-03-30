39c8a38907201e29ea9e0bb463ca4407cd30b489
pravinSThakur <thakur_pravin2002@yahoo.com>
2015-03-30 19:12:53 +0530

    Create Link.java

---
+++ Link.java
@@ -0,0 +1,12 @@
+package com.manage;
+
+public class Link
+{
+    public final Node target;
+    public final int weight;
+    public Link(Node argTarget, int argWeight)
+    { 
+    	target = argTarget; 
+    	weight = argWeight; 
+    }
+}
